f0687ff408a31be7c8fc51e467c56de561af3f60
Steve Degosserie <steve@parity.io>
2020-07-09 17:12:44 +0200

    Upgrade to v2.0.0-rc4 & bump version

--- src/mock.rs
+++ src/mock.rs
@@ -2,6 +2,8 @@ use crate::{Module, Trait};
 use frame_support::{
   impl_outer_origin, parameter_types, weights::Weight,
 };
+use frame_system as system;
+use pallet_timestamp as timestamp;
 use sp_core::{sr25519, Pair, H256};
 use sp_runtime::{
   testing::Header,
@@ -25,6 +27,7 @@ parameter_types! {
   pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
 }
 impl system::Trait for Test {
+  type BaseCallFilter = ();
   type Origin = Origin;
   type Call = ();
   type Index = u64;
