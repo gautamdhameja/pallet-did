1067c787df4885a990df15ddec90b166a45e65e2
Steve Degosserie <steve@parity.io>
2020-07-09 17:23:49 +0200

    Fix tests

--- src/mock.rs
+++ src/mock.rs
@@ -1,5 +1,7 @@
 use crate::{Module, Trait};
 use frame_support::{impl_outer_origin, parameter_types, weights::Weight};
+use frame_system as system;
+use pallet_timestamp as timestamp;
 use sp_core::{sr25519, Pair, H256};
 use sp_runtime::{
     testing::Header,
