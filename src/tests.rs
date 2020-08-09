4e9b7b7629d96bc461b47746508c2935c4301121
Jimmy Chu <jimmychu0807@gmail.com>
2020-08-09 13:23:50 +0800

    more comprehensive test

--- src/tests.rs
+++ src/tests.rs
@@ -3,7 +3,97 @@ use frame_support::{assert_noop, assert_ok};
 use sp_core::Pair;
 
 #[test]
-fn allows_owner_attr_update_tx() {
+fn allows_registering_did_with_valid_signature() {
+	new_test_ext().execute_with(|| {
+		let (did_pair, did_public) = account("My Organization");
+		let (satoshi_pair, satoshi_public) = account("Satoshi");
+		let reg_msg = b"did registration";
+
+		// Test 1: Test with invalid signature - should fail. The signature should be from
+		//   did private key, not Satoshi private key.
+		assert_noop!(DId::register_did(
+			Origin::signed(satoshi_public.clone()),
+			did_public,
+			reg_msg.to_vec(),
+			satoshi_pair.sign(reg_msg)
+		), Error::<Test>::InvalidSignature);
+
+		// Test 2: Test with valid signature - should succeed
+		assert_ok!(DId::register_did(
+			Origin::signed(satoshi_public.clone()),
+			did_public,
+			reg_msg.to_vec(),
+			did_pair.sign(reg_msg)
+		));
+
+		// Test 3: Test with DId registration the duplicate DId - should fail
+		assert_noop!(DId::register_did(
+			Origin::signed(satoshi_public.clone()),
+			did_public,
+			reg_msg.to_vec(),
+			did_pair.sign(reg_msg)
+		), Error::<Test>::DIdAlreadyExist);
+	})
+}
+
+#[test]
+fn change_did_owner() {
+	new_test_ext().execute_with(|| {
+		// Setup: Satoshi create a DId
+		let (did_pair, did_public) = account("My Organization");
+		let (_, satoshi_public) = account("Satoshi");
+		let reg_msg = b"did registration";
+		let del_type = b"del_type";
+
+		assert_ok!(DId::register_did(
+			Origin::signed(satoshi_public.clone()),
+			did_public,
+			reg_msg.to_vec(),
+			did_pair.sign(reg_msg)
+		));
+
+		// Test 1: Bob try to upsert DId delegate for Charles. This should fail
+		let (_, bob_public) = account("Bob");
+		let (_, charles_public) = account("Charles");
+
+		assert_noop!(DId::upsert_delegate(
+			Origin::signed(bob_public.clone()),
+			did_public,
+			del_type.to_vec(),
+			charles_public,
+			None
+		), Error::<Test>::NotOwner);
+
+		// Satoshi pass the DId ownership to Bob
+		assert_ok!(DId::change_owner(
+			Origin::signed(satoshi_public.clone()),
+			did_public,
+			bob_public,
+		));
+
+		// Test 2: Bob try to upsert DId delegate for Charles. This should succeed
+		assert_ok!(DId::upsert_delegate(
+			Origin::signed(bob_public.clone()),
+			did_public,
+			del_type.to_vec(),
+			charles_public,
+			None
+		));
+
+		// Test 3: Satoshi try to upsert DId delegate for Charles. This should fail, as he
+		//   is no longer the DId owner
+		assert_noop!(DId::upsert_delegate(
+			Origin::signed(satoshi_public.clone()),
+			did_public,
+			del_type.to_vec(),
+			charles_public,
+			None
+		), Error::<Test>::NotOwner);
+	})
+}
+
+#[test]
+fn accepts_owner_attr_update_tx() {
 	new_test_ext().execute_with(|| {
 		// Create a DId account
 		let (did_pair, did_public) = account("My Organization");
@@ -13,7 +103,7 @@ fn allows_owner_attr_update_tx() {
 
 		let reg_msg = b"did registration";
 
-		// Test: Should be able to register a new DId
+		// Register a DId
 		assert_ok!(DId::register_did(
 			Origin::signed(satoshi_public.clone()),
 			did_public,
@@ -21,7 +111,7 @@ fn allows_owner_attr_update_tx() {
 			did_pair.sign(reg_msg)
 		));
 
-		// Test: Should accept DId's owner signature
+		// Test #1: Should accept DId's owner signature
 		let name = b"attribute name";
 		let value = b"attribute value";
 		let dnvv_msg = DId::encode_dnvv(&did_public, name, value, None);
@@ -34,10 +124,10 @@ fn allows_owner_attr_update_tx() {
 		};
 		assert_ok!(DId::upsert_attribute_from_offchain_signature(
 			Origin::signed(satoshi_public.clone()),
-			tx_satoshi
+			tx_satoshi.clone()
 		));
 
-		// Test: Should not accept other ppl to update
+		// Test #2: Should not accept other ppl to update
 		let (bob_pair, bob_public) = account("Bob");
 		let tx_bob = AttributeUpdateTx {
 			did: did_public,
@@ -50,11 +140,26 @@ fn allows_owner_attr_update_tx() {
 			Origin::signed(bob_public.clone()),
 			tx_bob
 		), Error::<Test>::InvalidDelegate);
+
+		// Test #3: Should not accept Bob masquerading as Satoshi.
+		//   To test this, we need to give Bob upsert_attribute right first
+		assert_ok!(DId::upsert_delegate(
+			Origin::signed(satoshi_public.clone()),
+			did_public,
+			crate::OFFCHAIN_TX_DELEGATE_TYPE.to_vec(),
+			bob_public,
+			None
+		));
+
+		assert_noop!(DId::upsert_attribute_from_offchain_signature(
+			Origin::signed(bob_public.clone()),
+			tx_satoshi
+		), Error::<Test>::InvalidSignature);
 	});
 }
 
 #[test]
-fn allows_delegate_attr_update_tx() {
+fn accepts_delegate_attr_update_tx() {
 	new_test_ext().execute_with(|| {
 		System::set_block_number(1);
 
@@ -66,6 +171,7 @@ fn allows_delegate_attr_update_tx() {
 		let (bob_pair, bob_public) = account("Bob");
 		let reg_msg = b"did registration";
 
+		// Setup: Satoshi registers a DId
 		assert_ok!(DId::register_did(
 			Origin::signed(satoshi_public.clone()),
 			did_public,
@@ -75,7 +181,7 @@ fn allows_delegate_attr_update_tx() {
 
 		let valid_block_offset = 5;
 
-		// Test: Adding Bob as delegate should succeed
+		// Setup: Satoshi upsert Bob as DId delegate
 		assert_ok!(DId::upsert_delegate(
 			Origin::signed(satoshi_public.clone()),
 			did_public,
@@ -86,7 +192,7 @@ fn allows_delegate_attr_update_tx() {
 
 		System::set_block_number(1 + valid_block_offset);
 
-		// Test: Bob now be able to send attr_update_tx
+		// Test #1: Bob now should be able to send attr_update_tx
 		let name = b"attribute name";
 		let value = b"attribute value";
 		let msg = DId::encode_dnvv(&did_public, name, value, None);
@@ -102,9 +208,8 @@ fn allows_delegate_attr_update_tx() {
 			tx_bob.clone()
 		));
 
-		// Now, Bob valid period has passed
+		// Test #2: Bob valid period has passed. Now the transaction should fail.
 		System::set_block_number(2 + valid_block_offset);
-		// Test: This tx should fail
 		assert_noop!(DId::upsert_attribute_from_offchain_signature(
 			Origin::signed(bob_public.clone()),
 			tx_bob
